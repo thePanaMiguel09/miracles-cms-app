f896607c59f820acf4dcbd50d2f861e556c62a87
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2026-01-26 17:07:34 -0500

    Login form implemented

--- src/presentation/screens/LoginScreen.tsx
+++ src/presentation/screens/LoginScreen.tsx
@@ -1,6 +1,8 @@
 import Ionicons from "@expo/vector-icons/Ionicons";
-import React from "react";
+import React, { useRef } from "react";
 import {
+  KeyboardAvoidingView,
+  Platform,
   Pressable,
   Text,
   TextInput,
@@ -9,63 +11,124 @@ import {
 } from "react-native";
 
 import { SafeAreaView } from "react-native-safe-area-context";
+
+import { Controller } from "react-hook-form";
+
 import { useLogin } from "../hooks/use-login";
 
 function LoginScreen() {
-  const { showPassword, handleShowPassword } = useLogin();
+  const firtInputRef = useRef<TextInput>(null);
+  const secondInputRef = useRef<TextInput>(null);
+
+  const {
+    control,
+    showPassword,
+    handleShowPassword,
+    register,
+    errors,
+    isLoading,
+    handleSubmit,
+    onSubmit,
+  } = useLogin();
 
   return (
-    <SafeAreaView>
-      <View className="w-screen h-screen p-5">
-        <View className="w-full h-full flex items-center justify-evenly flex-col p-4">
-          <View>
-            <Ionicons name="cart-outline" className="text-center" size={60} />
-            <Text className="font-semibold">Miracles CMS</Text>
-          </View>
+    <SafeAreaView className="flex-1">
+      <KeyboardAvoidingView
+        className="flex-1"
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        keyboardVerticalOffset={Platform.OS === "ios" ? 0 : 20}
+      >
+        <View className="flex-1 p-5">
+          <View className="w-full h-full flex items-center justify-evenly flex-col p-4">
+            <View>
+              <Ionicons name="cart-outline" className="text-center" size={60} />
+              <Text className="font-semibold">Miracles CMS</Text>
+            </View>
 
-          <View className="w-full h-[60%] flex flex-col justify-evenly">
-            <View className="flex w-full flex-col items-start">
-              <Text className="text-3xl font-semibold">Inicio de Sesión</Text>
-              <Text className="text-sm">Hola! Genial verte de nuevo</Text>
-            </View>
-            <View>
-              <Text className="text-red-400">Correo</Text>
-              <TextInput
-                className="border-b-2 border-gray-300"
-                placeholder="example@email.com"
-              />
-            </View>
-            <View>
-              <Text className="text-red-400">Contraseña</Text>
-              <View className="relative">
-                <TextInput
-                  secureTextEntry={showPassword}
-                  placeholder="********"
-                  className="border-b-2 border-gray-300 pr-10 py-2"
+            <View className="w-full h-[60%] flex flex-col justify-evenly">
+              <View className="flex w-full flex-col items-start">
+                <Text className="text-3xl font-semibold">Inicio de Sesión</Text>
+                <Text className="text-sm">Hola! Genial verte de nuevo</Text>
+              </View>
+              <View>
+                <Text className="text-red-400">Correo</Text>
+                <Controller
+                  control={control}
+                  name="email"
+                  render={({ field: { onChange, onBlur, value } }) => (
+                    <TextInput
+                      ref={firtInputRef}
+                      className="border-b-2 border-gray-300"
+                      placeholder="example@email.com"
+                      keyboardType="email-address"
+                      autoCapitalize="none"
+                      returnKeyType="next"
+                      onChangeText={onChange}
+                      value={value}
+                      onBlur={onBlur}
+                      onSubmitEditing={() => {
+                        secondInputRef.current?.focus();
+                      }}
+                    />
+                  )}
                 />
 
-                <Pressable
-                  onPress={handleShowPassword}
-                  className="absolute right-0 top-1/2 -translate-y-1/2"
-                >
-                  <Ionicons
-                    name={!showPassword ? "eye-off-outline" : "eye-outline"}
-                    size={22}
-                    color="#6B7280"
-                  />
-                </Pressable>
+                {errors.email && (
+                  <Text className="text-sm text-red-600 font-light">
+                    {errors.email.message}
+                  </Text>
+                )}
               </View>
-            </View>
-            <View className="w-full flex flex-row justify-center">
-              <TouchableOpacity className="w-[70%] bg-red-400 rounded-md">
-                <Text className="font-semibold text-white text-center m-3">
-                  Ingresar
-                </Text>
-              </TouchableOpacity>
+              <View>
+                <Text className="text-red-400">Contraseña</Text>
+                <View className="relative">
+                  <Controller
+                    control={control}
+                    name="password"
+                    render={({ field: { onChange, value } }) => (
+                      <TextInput
+                        ref={secondInputRef}
+                        secureTextEntry={showPassword}
+                        placeholder="********"
+                        className="border-b-2 border-gray-300 pr-10 py-2"
+                        returnKeyType="done"
+                        onChangeText={onChange}
+                        value={value}
+                      />
+                    )}
+                  />
+
+                  <Pressable
+                    onPress={handleShowPassword}
+                    className="absolute right-0 top-1/2 -translate-y-1/2"
+                  >
+                    <Ionicons
+                      name={!showPassword ? "eye-off-outline" : "eye-outline"}
+                      size={22}
+                      color="#6B7280"
+                    />
+                  </Pressable>
+                </View>
+                {errors?.password && (
+                  <Text className="text-sm text-red-600 font-light">
+                    {errors.password.message}
+                  </Text>
+                )}
+              </View>
+              <View className="w-full flex flex-row justify-center">
+                <TouchableOpacity
+                  onPress={handleSubmit(onSubmit)}
+                  className="w-[70%] bg-red-400 rounded-md"
+                >
+                  <Text className="font-semibold text-white text-center m-3">
+                    {!isLoading ? "Ingresar" : "Cargando"}
+                  </Text>
+                </TouchableOpacity>
+              </View>
             </View>
           </View>
         </View>
-      </View>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
