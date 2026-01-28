edaa67f9e9e07662656aae54e8a03f7a52804e19
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2026-01-28 17:59:44 -0500

    ProductScreens created

---
+++ src/presentation/screens/ProductsScreen.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+function ProductsScreen() {
+  return (
+    <SafeAreaView>
+      <Text>Products screens</Text>
+    </SafeAreaView>
+  );
+}
+
+export default ProductsScreen;
