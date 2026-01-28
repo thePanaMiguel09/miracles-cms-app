a98c199106fbe77e64ea708b7a9c483924ee3403
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2026-01-28 18:00:19 -0500

    unused files deleted

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     "./app/**/*.{js,jsx,ts,tsx}",
     "./src/presentation/**/*.{js,jsx,ts,tsx}",
+    "./**/*.{js,jsx,ts,tsx}",
   ],
   presets: [require("nativewind/preset")],
   theme: {
