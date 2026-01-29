a2b7ccb84baa5d14570a53c4b614fc3cdb989491
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2026-01-29 14:13:57 -0500

    UsersScreens created

--- src/presentation/screens/UsersScreen.tsx
+++ src/presentation/screens/UsersScreen.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import HeaderView from "../ui/shared/HeaderView";
 
 function UsersScreen() {
   return (
     <SafeAreaView className="flex-1">
-      <Text>Users screen</Text>
+      <HeaderView title="Usuarios" icon="add" />
     </SafeAreaView>
   );
 }
