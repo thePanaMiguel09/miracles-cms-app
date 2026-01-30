2e28829e50e6f3ed187039cddbd0860302757e7a
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2026-01-30 15:32:01 -0500

    axios interceptor implemented in axios instance for adding authorization header and avoid 401 response from server

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,7 +1,19 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
-
-
 
 export const api = axios.create({
     baseURL: process.env.EXPO_PUBLIC_API_ULR,
 });
+
+
+api.interceptors.request.use(
+    async (config) => {
+        const token = await AsyncStorage.getItem('token');
+        console.log(token)
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config;
+    }, (error) => Promise.reject(error)
+);
