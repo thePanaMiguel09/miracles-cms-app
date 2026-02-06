e91d58c94e4cf2f9fc4098927ab44bcb27c39fe4
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2026-02-06 16:04:42 -0500

    Custom input created for reused on Screend forms

---
+++ src/presentation/ui/shared/CustomInput.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Controller } from "react-hook-form";
+import { KeyboardTypeOptions, Text, TextInput, View } from "react-native";
+
+interface CustomInputProps {
+  name: string;
+  control: any;
+  placeholder: string;
+  inputType: KeyboardTypeOptions;
+  error: string | undefined;
+  autoCapitalizeOptions?:
+    | "words"
+    | "none"
+    | "sentences"
+    | "characters"
+    | undefined;
+}
+
+const CustomInput = ({
+  name,
+  control,
+  error,
+  inputType,
+  placeholder,
+  autoCapitalizeOptions,
+  ...rest
+}: CustomInputProps) => {
+  return (
+    <View className="mb-2">
+      <Controller
+        name={name}
+        control={control}
+        render={({ field: { onChange, value, onBlur } }) => (
+          <TextInput
+            autoCapitalize={autoCapitalizeOptions}
+            keyboardType={inputType}
+            placeholder={placeholder}
+            className="bg-gray-200/95 border-b border-gray-400 rounded-sm"
+            onChangeText={onChange}
+            value={value?.toString()}
+            onBlur={onBlur}
+            {...rest}
+          />
+        )}
+      />
+      {error && <Text className="text-xs text-red-500 mt-1">{error}</Text>}
+    </View>
+  );
+};
+
+export default CustomInput;
