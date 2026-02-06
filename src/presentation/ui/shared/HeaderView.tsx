aeb05be89d1c0d95154dc7d757a0032d5e674fb5
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2026-02-06 16:05:49 -0500

    Screens HederView custom component created for reused on screens

--- src/presentation/ui/shared/HeaderView.tsx
+++ src/presentation/ui/shared/HeaderView.tsx
@@ -8,12 +8,12 @@ interface HeaderViewProps {
   onPress?: () => void;
 }
 
-function HeaderView({ title, icon }: HeaderViewProps) {
+function HeaderView({ title, icon, onPress }: HeaderViewProps) {
   return (
     <View className="h-[10%] p-4 flex-row justify-between items-center m-4 bg-white rounded-lg shadow-xl shadow-black/50 ">
       <Text className="font-semibold text-2xl">{title}</Text>
       {icon && (
-        <Pressable>
+        <Pressable onPress={onPress}>
           <Ionicons size={24} name="add-outline" />
         </Pressable>
       )}
