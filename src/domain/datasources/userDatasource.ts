7b4e987f1a56a2d91cf09ebcdb00e4f93a60713e
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2026-02-11 21:11:02 -0500

    fetchUser by id contract added to UserDataSource in domain layer

--- src/domain/datasources/userDatasource.ts
+++ src/domain/datasources/userDatasource.ts
@@ -3,4 +3,5 @@ import { User, UserRegister } from "../entities/user";
 export abstract class UserDatasource {
     abstract fetchUsers(): Promise<User[]>
     abstract createUser(user: UserRegister): Promise<void>;
+    abstract fetchUser(id: number): Promise<User | null>;
 }
