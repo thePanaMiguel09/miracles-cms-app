e50aed1347c9ece9285e3606b116434b1823ae76
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2026-02-11 20:51:43 -0500

    user domain entity atributes corrected

--- src/domain/entities/user.ts
+++ src/domain/entities/user.ts
@@ -1,18 +1,18 @@
 export class User {
 
     constructor(
-        readonly userId: number,
         readonly userDNI: number,
         readonly email: string,
         readonly names: string,
         readonly surnames: string,
         readonly phoneNumber: number,
-        readonly roleId: number,
-        readonly storeId: number,
+        readonly userState: boolean,
         readonly createdAt: Date,
-        readonly userState?: boolean,
+        readonly storeId: number | null,
+        readonly roleId?: number,
+        readonly roleName?: string,
+        readonly userId?: number,
     ) { }
-
 }
 
 export class UserRegister {
