87b0978b614bf4e0ca9d9537312fa0b1fe41ef24
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2026-02-11 20:53:13 -0500

    fetchUser by user id function added to userRepository contract and fetchUserByIdUseCase created

--- src/domain/repositories/userRepository.ts
+++ src/domain/repositories/userRepository.ts
@@ -3,4 +3,5 @@ import { User, UserRegister } from "../entities/user";
 export abstract class UserRepository {
     abstract fetchUsers(): Promise<User[]>
     abstract createUser(data: UserRegister): Promise<void>;
+    abstract fetchUser(id: number): Promise<User | null>;
 }
---
+++ src/domain/usecases/fetchUserByIdUseCase.ts
@@ -0,0 +1,14 @@
+import { UserRepository } from "../repositories/userRepository";
+
+export class FecthUserByIdUseCase {
+    constructor(private readonly userRepository: UserRepository) { }
+
+    async execute(id: number): Promise<ReturnType<UserRepository['fetchUser']>> {
+        if (typeof id !== 'number' || typeof id === 'undefined') {
+            throw new Error('Invalid user id. User id must be a number.');
+        }
+
+        return await this.userRepository.fetchUser(id);
+    }
+
+}
