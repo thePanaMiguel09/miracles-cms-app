bf05b3e6755ca9c37fc2aafb66fc6ba161f02b43
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2026-02-12 23:34:00 -0500

    CustomModal type and onConfirmation added to props

--- src/presentation/ui/shared/CustomModal.tsx
+++ src/presentation/ui/shared/CustomModal.tsx
@@ -2,21 +2,25 @@ import Ionicons from "@expo/vector-icons/Ionicons";
 import React from "react";
 import { Modal, Text, TouchableOpacity, View } from "react-native";
 
-interface CustomeModalProps {
+interface CustomModalProps {
   modalTitle: string;
   modalMessage: string;
   modalIcon?: keyof typeof Ionicons.glyphMap;
   visible: boolean;
   onClose: () => void;
+  type: "confirmation" | "information";
+  onConfirmation?: () => void;
 }
 
-const CustomeModal = ({
+const CustomModal = ({
   visible,
   onClose,
   modalMessage,
   modalTitle,
   modalIcon,
-}: CustomeModalProps) => {
+  type,
+  onConfirmation,
+}: CustomModalProps) => {
   return (
     <Modal
       transparent
@@ -39,18 +43,38 @@ const CustomeModal = ({
 
           <Text className="text-center text-gray-600 mb-4">{modalMessage}</Text>
 
-          <TouchableOpacity
-            onPress={onClose}
-            className="bg-red-400 py-2 rounded-lg"
-          >
-            <Text className="text-white text-center font-semibold">
-              Aceptar
-            </Text>
-          </TouchableOpacity>
+          {type === "information" && (
+            <TouchableOpacity
+              onPress={onClose}
+              className="bg-red-400 py-2 rounded-lg"
+            >
+              <Text className="text-white text-center font-semibold">
+                Aceptar
+              </Text>
+            </TouchableOpacity>
+          )}
+          {type === "confirmation" && (
+            <View className="bg-blue-300 flex flex-row items-center justify-center">
+              <TouchableOpacity
+                onPress={onClose}
+                className="m-1 p-2 border border-red-400 rounded-lg"
+              >
+                <Text className="text-red-400">Cancelar</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => {
+                  onConfirmation?.();
+                }}
+                className="m-1 p-2 border border-red-400 rounded-lg bg-red-400"
+              >
+                <Text className="text-white">Aceptar</Text>
+              </TouchableOpacity>
+            </View>
+          )}
         </View>
       </View>
     </Modal>
   );
 };
 
-export default CustomeModal;
+export default CustomModal;
