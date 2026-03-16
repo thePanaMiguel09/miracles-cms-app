251ee66977aa0c70b4c7a311926347c602cc6e82
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2026-03-15 21:03:05 -0500

    userIUpdateDTO implemented on UserDatasource

--- src/domain/datasources/userDatasource.ts
+++ src/domain/datasources/userDatasource.ts
@@ -1,8 +1,14 @@
+import { UserUpdateDTO } from "../dtos/userupdate.dto";
 import { User, UserRegister } from "../entities/user";
 
 export abstract class UserDatasource {
-    abstract fetchUsers(): Promise<User[]>
     abstract createUser(user: UserRegister): Promise<void>;
+    
+    abstract fetchUsers(): Promise<User[]>
     abstract fetchUser(id: number): Promise<User | null>;
+    
     abstract updateUserState(id: number, state: boolean): Promise<void>;
+    abstract updateUserCommerce(id: number, commerceId: number | null): Promise<void>;
+    
+    abstract updateUserInformation(id: number, data: UserUpdateDTO): Promise<void>;
 }
